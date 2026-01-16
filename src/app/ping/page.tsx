3a4d4a4deaf4b4c20442c7fc026534ef8d3a66aa
TareSY <128743007+TareSY@users.noreply.github.com>
2026-01-16 12:06:19 -0500

    Delete page.tsx

--- src/app/ping/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function PingPage() {
-    return (
-        <div className="flex items-center justify-center min-h-screen text-4xl font-bold text-white bg-black">
-            PONG 🏓
-        </div>
-    );
-}
